1f0ab5223765aeb311296d611c20ee177eda5b5b
lamhieuyeuem-svg <lamhieuyeuem@gmail.com>
2026-09-07 10:27:29 +0700

    Add files via upload

---
+++ LayMa.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Layma.net Bypass - CUTTAY PROMAX
+// @namespace    http://tampermonkey.net/
+// @version      1.0.7
+// @description  Auto nhiệm vụ lấy mã
+// @author       DuckXkurumi
+// @icon         https://files.catbox.moe/8p9l8w.jpg
+// @match        https://layma.net/*
+// @connect      *
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/lamhieuyeuem-svg/Bypass_LayMa/refs/heads/main/BypassLayma.js
+// @updateURL    https://github.com/duckpjvzexe/projectscript112247/raw/refs/heads/main/layma_net/Auto_LaymaV2.user.js
+// @downloadURL  https://github.com/duckpjvzexe/projectscript112247/raw/refs/heads/main/layma_net/Auto_LaymaV2.user.js
+// ==/UserScript==
